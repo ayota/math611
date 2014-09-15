f641ffb2cee4de393aa62a702b78a6106cc8a266
ayota <ayota@outlook.com>
2014-09-15 11:18:44 -0400

    updated with answer to 3A

--- hwk2.3.r
+++ hwk2.3.r
@@ -1,1 +1,29 @@
-test
+library(compiler)
+library(microbenchmark)
+
+##611 HWK 2##
+qnorm(.95)
+qnorm(.975)
+qnorm(.99)
+
+###HWK 2, 3A###
+#p is the desired probability we are looking for x for
+inverse <- function(p, step, start) {
+  #initialize all the things!
+  norm<-function(x){1/sqrt(2*pi)*exp(-x^2/2)} 
+  i <- start + step
+  area <- 0
+  
+  #integrate function
+  while (area < p) {
+    integrand <- integrate(norm,lower=-Inf,upper=i)
+    area <- integrand$value
+    xvalue <- i
+    i <- i + step
+  }
+  
+  return(xvalue)
+}
+
+##timing##
+compare <- microbenchmark(inverse(.975), qnorm(.975), times = 10)
