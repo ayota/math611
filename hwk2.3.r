694de555a405f2055cfb7f93445bdf13d1391098
ayota <ayota@outlook.com>
2014-09-15 19:42:22 -0400

    Edited 3b and 3c

--- hwk2.3.r
+++ hwk2.3.r
@@ -1,10 +1,6 @@
 library(compiler)
 library(microbenchmark)
-
-##611 HWK 2##
-qnorm(.95)
-qnorm(.975)
-qnorm(.99)
+library(sm)
 
 ###HWK 2, 3A###
 #p is the desired probability we are looking for x for
@@ -29,19 +25,27 @@ inverse <- function(p, step, start) {
 compare <- microbenchmark(inverse(.975), qnorm(.975), times = 10)
 
 ###Question 3b###
-
 sampler <- function(N) {
   #Set seed and generate vector of N random RVs
   set.seed(100)
   results <- matrix(0,N,3)
   results[,1] <- as.vector(runif(N,min=0,max=1))
-  results[,2] <- sapply(results[,1], FUN = inverse, step=.01,start=-3)
+  results[,2] <- sapply(results[,1], FUN = inverse, step=.005,start=-5)
   results[,3] <- sapply(results[,1], FUN =qnorm)
   return(results)
 }
 
-qqnorm(results[,2])
-qqline(results[,2],col="red")
+sample <- sampler(10000)
+
+hist(sample[,2], col="red")
+hist(sample[,3], col="blue", add=T)
+plot(density(sample[,2]),col="blue")
+plot(density(sample[,3]), col="red")
+
+class(sample[,2])
+
+qqnorm(sample[,2])
+qqline(sample[,2],col="red")
 
 ###Question 3c###
 #Overly favors postive Q over negative Q
@@ -49,25 +53,35 @@ qqline(results[,2],col="red")
 #N is the desired number of samples
 genNorm <- function(C, N){
   nvars <- c()
+  
   while(length(nvars) < N){
-    u = C*runif(1); 
-    y = rexp(1,rate=1)
-    q = -rexp(1, rate=1)
-    if(u < exp(y-(y^2/2))/sqrt(2*pi)) {
-      nvars <- c(nvars,y)
+    u <- C*runif(1); 
+    expvars <- runif(1)
+    
+    if (expvars > .5) {
+      y = rexp(1,rate=1)
+    } else {
+      y = -rexp(1, rate=1)
     }
     
-    if(u < exp(q-(q^2/2))/sqrt(2*pi)) {
-      nvars <- c(nvars,q)
+    ##redo the math here
+    if(u < exp((y^2/2)-y)/sqrt(2*pi)) {
+      nvars <- c(nvars,y)
     }
+
   }
   return(nvars)
 }
 
-vars <- genNorm(1,10000)
-nvars <- rnorm(10000)
+vars <- genNorm(2,10000)
+rnormals <- rnorm(10000)
 
-hist(vars)
-hist(nvars)
+hist(vars, col = "red")
+hist(rnormals, add=T)
+
+plot(density(vars), col="red")
+plot(density(rnormals))
+
 qqnorm(vars)
 qqline(vars, col="red")
+
