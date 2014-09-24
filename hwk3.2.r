5e21a7ad2296f4d5d03ca9aef66cacc4584b05f9
ayota <ayota@outlook.com>
2014-09-24 12:19:40 -0400

    Cleaning up function, adding mechanism to check accuracy of steepest ascent for gmm

--- hwk3.2.r
+++ hwk3.2.r
@@ -1,48 +1,42 @@
 ###part a###
-##initialize all the things!##
-p1 <- .7
-p2 <- .3
-mu1 <- 0
-mu2 <- 3
-var1 <- 1
-var2 <- 4
+##this function generates a gaussian mixture model.
+#Input: N, the number of RVs desired; a set of parameters for each normal, in the form c(p1,p2,mu1,mu2,var1,var2)
+#Output: A vector of Xi's
 
-GMM <- function(N = 100, p1 = .7, p2 = .3, mu1 = 0, mu2 = 3, var1 = 1, var2 = 4) {
-  X <- matrix(0, N, 1)
-  n1 = n2 = 0
+params <- c(0, 3, 1, 4,.7)
+
+GMM <- function(N = 100, params = c(0, 3, 1, 4,.7)) {
+  #generate 100 uniform RVs to represent test for normal 1 or normal 2
+  set.seed(100)
+  X <- as.vector(runif(N))
   
-  for(i in 1:N) {
-    prob <- runif(1)
-    if(prob < p1) {
-      X[i, 1] <- rnorm(1, mean = mu1, sd = sqrt(var1) )
-    } else {
-      X[i, 1] <- rnorm(1, mean = mu2, sd = sqrt(var2) )
-    }
-  }
+  #now apply the condition if uniform < p1, we assign normal 1, if greater assign normal 2
+  set.seed(100)
+  sapply(X, FUN = function(x) {if (x < params[5]) rnorm(1, mean = params[1], sd = sqrt(params[3])) else rnorm(1, mean = params[2], sd = sqrt(params[4]))})
+  
   return(X)
 }
 
-samples <- GMM(100, p1, p2, mu1, mu2, var1, var2)
+samples <- GMM(100, params)
 
 histsamples <- hist(samples, breaks = 10)
 
-###part b, i###
-
-
-
 ###part b, ii: steepest ascent algorithm###
+#this finds the mle estimator for a given function
 
 ##test function for f(x1,x2) = -(10-x1)^2 - (5-x2)^2
-
 ##initialize the function
-#x is a vector of param x1 and x2
+#Input: x is a vector of param x1 and x2
+#Output: single value y
 testfunction <- function(x) {
   y <- -(10-x[1])^2 - (5 - x[2])^2
   return(y)
 }
 
 ##function to calculate the gradient
-#x is a vector of params x1 and x2
+#Input: x is a vector of params x1 and x2
+#Output: a vector of two values representing the partial for x1 and x2
+
 gradient <- function(x) {
   df.dx1 <- (20 - 2*x[1])
   df.dx2 <- (10 - 2*x[2])
@@ -51,6 +45,8 @@ gradient <- function(x) {
 }
 
 ##function to calculate the norm of the gradient
+#Input: a gradient vector
+#Output: single numeric value
 norm <- function(grad) {
   gradnorm = sqrt(sum(grad^2))
   return(gradnorm)
@@ -61,10 +57,9 @@ norm <- function(grad) {
 #starts <- start values for all params, as a vector
 #target <- some sufficiently small value
 #step <- how far each step moves
+##output: a path for x1/x2 to maximizers, last value is the max
 
-##output: a path for x1/x2 to maximizers
-
-ascent <- function(starts = c(0,0), target = .01, step = .1) {
+testascent <- function(starts = c(0,0), target = .01, step = .1) {
   #initializing
   x.new <- starts
   grad.new <- gradient(x.new)
@@ -97,16 +92,38 @@ ascent <- function(starts = c(0,0), target = .01, step = .1) {
   return(rbind(path,x.new))
 }
 
-test <- ascent()
-test
+test <- testascent()
 
 ##steepest ascent for two component gmm
 ##gmm of two normal RVs: N(mu1,var1) w/ prob of p1 & N(mu2,var2) w/ prob p2
 
-
-##initialize the function
+##log likelihood function
 #params to optimize: mu1, mu2, var1, var2, p1, p2
 #because p1 + p2 = 1, we define p1 = p, p2 = 1 - p
+#Input: Vector of params in form (mu1,mu2,var1,var2,p); vector of Xi's
+#loglikelihood estimate L(theta)
+
+loglike <- function(params, samples = samples) {
+  #(this is not necessary, it is just so I don't get confused)
+  mu1 <- params[1]
+  mu2 <- params[2]
+  var1 <- params[3]
+  var2 <- params[4]
+  P1 <- params[5]
+  P2 <- 1-P1
+  
+  #define the log-likelihood function without the summation
+  loglike.fxn <- function(x) {
+    Y <- log( P1*( (sqrt(2*pi*var1))^(-1) * exp( -(x-mu1)^(2) * (2*var1)^(-1)) ) + P2*( (sqrt(2*pi*var2))^(-1) * exp( -(x-mu2)^(2) * (2*var2)^(-1)) ) )
+    return(Y)
+  }
+  
+  #apply the new function for all the Xi's
+  L.theta <- sum(sapply(samples, FUN = loglike.fxn))
+  
+  #return the sum (aka L(theta))
+  return(L.theta)
+}
 
 ##function to calculate the gradient
 #samples is a vector of 100 x's calculated using GMM fxn
@@ -179,6 +196,7 @@ testnorm <- norm(testgrad)
 #step <- how far each step moves
 
 ##output: a path for x1/x2 to maximizers
+samples <- GMM(100, p1, p2, mu1, mu2, var1, var2)
 
 ascent <- function(starts = c(-.5,2,0,3,.4), target = .01, step = .1) {
   #initializing
@@ -192,7 +210,7 @@ ascent <- function(starts = c(-.5,2,0,3,.4), target = .01, step = .1) {
     
     #test to make sure gradient is continuing to get smaller, if not, we decrease step
     if (norm(grad.new) %in% norms) {
-      step <- step/2
+      step <- step/10
     }
     
     path <- rbind(path, x.new)  #add new x to previous path
@@ -216,8 +234,8 @@ ascent <- function(starts = c(-.5,2,0,3,.4), target = .01, step = .1) {
   return(rbind(path,x.new))
 }
 
-testascent <- ascent(starts = c(0,0,1,1,0), target = .05, step = .0001)
+testascent <- ascent(starts = c(-.1,2.8,.8,3.8,.5), target = .05, step = .0001)
 
-testascent
+fhtestascent
 debug(ascent)
 
